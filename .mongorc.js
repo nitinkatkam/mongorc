5e186ea4348dd17914df0793d3bebd2bf1cf1825
nitinkatkam <nitinkatkam@gmail.com>
2020-10-25 19:18:41 +0400

    Changed soft link to a hard link

--- .mongorc.js
+++ .mongorc.js
@@ -1,1 +1,11 @@
-/Users/nitin/.mongorc.js
+EDITOR='vi';
+
+function whoami() {
+				print(tojson(db.runCommand({connectionStatus: 1})))
+} //should we also have a showPrivileges: true ?
+
+whoAmI = whoami;
+
+//Enable pretty print by default
+DBQuery.prototype._prettyShell = true;
+
